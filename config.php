96f70ff5b0911fba7635154d60a4add5db209fde
EagleTech <45262123+noproblematall@users.noreply.github.com>
2021-04-26 07:51:25 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 // Phaxio config --------------
-$apiKeys['live'] = 'ngkgwd1fr3kreq1b9xrz1i007qc4bhi8c0kx3i70';
-$apiSecrets['live'] = 'ppq65vo36opugqqhpy0tlht1z0609i8gr0gcloud';
-$apiKeys['test'] = 'lmi55pdwczknexmhf1n3sxw0u45trrdmgzso95oo';
-$apiSecrets['test'] = 'dojj39ycghj0foa99jnip5yv25mh2ll5neowwmr4';
+$apiKeys['live'] = '';
+$apiSecrets['live'] = '';
+$apiKeys['test'] = '';
+$apiSecrets['test'] = '';
 $apiMode = 'live';
 $apiHost = 'https://api.phaxio.com/v2/';
 
-$phaxio_phone = '+12157544374';
+$phaxio_phone = '';
 
 // Text-magic config ------------
 
-$textmagic_key = 'ITq4EYCCxXlSBj5JFYlUxgMGR4dOjD';
-$textmagic_name = 'davidlynn1';
-// $textmagic_phone = '+12156876380';
+$textmagic_key = '';
+$textmagic_name = '';
+// $textmagic_phone = '';
